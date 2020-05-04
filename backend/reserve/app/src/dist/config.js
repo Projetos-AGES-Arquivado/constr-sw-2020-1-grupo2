6130560165fc6823d8e657849c251abf285a4172
Hercilio <hercilio.ortiz@acad.pucrs.br>
2020-05-04 19:13:21 -0300

    Reserve APP conectado com Mongo com sucesso

--- backend/reserve/app/src/dist/config.js
+++ backend/reserve/app/src/dist/config.js
@@ -1,8 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongodata = {
+    username: process.env.APP_MONGO_USER,
+    password: process.env.APP_MONGO_PASS,
+    port: process.env.MONGO_PORT,
+    dbname: process.env.MONGO_DB
+};
 exports.default = {
     development: {
-        db_url: process.env.MONGO_URI,
+        db_url: `mongodb://${mongodata.username}:${mongodata.password}@reserve_mongo:${mongodata.port}/${mongodata.dbname}`,
     },
     homolog: {
         db_url: process.env.MONGO_URI,
--- backend/reserve/app/src/src/config.ts
+++ backend/reserve/app/src/src/config.ts
@@ -1,8 +1,14 @@
 /* eslint-disable @typescript-eslint/camelcase */
+const mongodata = {
+  username: process.env.APP_MONGO_USER,
+  password: process.env.APP_MONGO_PASS,
+  port: process.env.MONGO_PORT,
+  dbname: process.env.MONGO_DB
+};
 
 export default {
   development: {
-    db_url: process.env.MONGO_URI,
+    db_url: `mongodb://${mongodata.username}:${mongodata.password}@reserve_mongo:${mongodata.port}/${mongodata.dbname}`,
   },
   homolog: {
     db_url: process.env.MONGO_URI,
