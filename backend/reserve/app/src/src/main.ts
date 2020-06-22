85e2ee117de8e27f24fda0f7f25848a95d184d21
Hercilio <hercilio.ortiz@acad.pucrs.br>
2020-06-22 20:40:08 -0300

    cors adicionado

--- backend/reserve/app/src/src/main.ts
+++ backend/reserve/app/src/src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
   app.setGlobalPrefix('reserves');
 
   const options = new DocumentBuilder()
